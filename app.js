583ed246e37fd930900e47baf08e52f7786ea60e
Jonathan Alvarado <jonalobo98@gmail.com>
2022-07-21 17:09:38 -0600

    Ajuste de cors

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const cors = require('cors')
+
 const rutaProductos = require('./routes/products')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.static('public'))
 
